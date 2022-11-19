fa7bd649bd21e473fc07720eb1d1acd8f5326461
smohebbi <siavash.mohebbi@gmail.com>
2022-11-19 14:46:45 +0100

    resolved warning

--- src/Contact.js
+++ src/Contact.js
@@ -13,7 +13,7 @@ class Contact extends React.Component {
   authorize(e) {
     const password = e.target.querySelector(
       'input[type="password"]').value;
-    const auth = password == this.state.password;
+    const auth = password === this.state.password;
     this.setState({
       authorized: auth
     });
